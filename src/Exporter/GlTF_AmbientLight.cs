7583bbdfbac03b05b839e3442fbfc9e87c75a24c
dtysky <dtysky@outlook.com>
2018-09-07 12:05:14 +0800

    Fix: AmbienLight

--- src/Exporter/GlTF_AmbientLight.cs
+++ src/Exporter/GlTF_AmbientLight.cs
@@ -5,18 +5,25 @@ using System.Collections;
 public class GlTF_AmbientLight : GlTF_Light {
     public GlTF_AmbientLight() { type = "ambient"; }
 
-	public override void Write()
-	{
+    public override void Write()
+    {
         Indent();
         jsonWriter.Write("{\n");
         IndentIn();
         color.Write();
         jsonWriter.Write(",\n");
         Indent();
+        double ins = intensity;
+        if (quadraticAttenuation)
+        {
+            ins *= 3;
+        }
+        jsonWriter.Write("\"intensity\": " + ins + ",\n");
+        Indent();
         jsonWriter.Write("\"type\": \"" + type + "\"\n");
         IndentOut();
         Indent();
         jsonWriter.Write("}");
-	}
+    }
 }
 #endif
