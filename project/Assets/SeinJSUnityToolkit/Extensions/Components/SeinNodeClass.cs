4777735d7dc7ef938c741f05b47b425d9ad4aa58
dtysky <dtysky@outlook.com>
2019-10-23 20:52:12 +0800

    Update

---
+++ project/Assets/SeinJSUnityToolkit/Extensions/Components/SeinNodeClass.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+namespace SeinJS
+{
+    public enum ESeinNodePropType
+    {
+        Num,
+        Arr,
+        Str,
+        Bool,
+        Tex2D,
+        Vec2,
+        Vec3,
+        Vec4,
+        Mat2,
+        Mat3,
+        Mat4
+    }
+}
+
+[AddComponentMenu("Sein/Class/Sein Node Class")]
+public class SeinNodeClass : MonoBehaviour
+{
+    public virtual string className { get; }
+    public object props;
+
+
+    //public virtual JProperty Serialize(SeinJS.ExporterEntry entry)
+    //{
+
+    //}
+}
---
+++ project/Assets/TestActor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+[AddComponentMenu("Sein/Class/Test Actor")]
+public class TestActor : SeinNodeClass
+{
+    [System.Serializable]
+    public struct TestActorProps
+    {
+        public int a;
+        public string b;
+    }
+
+    public override string className { get { return "TestActor"; } }
+    public new TestActorProps props;
+}
