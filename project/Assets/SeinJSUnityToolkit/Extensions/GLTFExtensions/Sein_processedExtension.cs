4d863cce500658256d75113671923320a4fa3f3d
dtysky <dtysky@outlook.com>
2022-07-22 16:28:27 +0800

    Add Sein_processedExtension

---
+++ project/Assets/SeinJSUnityToolkit/Extensions/GLTFExtensions/Sein_processedExtension.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * @File   : Sein_processedExtension.cs
+ * @Author : dtysky (dtysky@outlook.com)
+ * @Link   : dtysky.moe
+ * @Date   : 2019/10/10 0:00:00PM
+ */
+using GLTF.Schema;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace SeinJS
+{
+    public class Sein_processedExtension : Extension
+    {
+        public JProperty Serialize()
+        {
+            return new JProperty(
+                ExtensionManager.GetExtensionName(typeof(Sein_processedExtensionFactory)),
+                new JObject()
+            );
+        }
+    }
+}
---
+++ project/Assets/SeinJSUnityToolkit/Extensions/GLTFExtensions/Sein_processedExtensionFactory.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * @File   : Sein_processedExtensionFactory.cs
+ * @Author : dtysky (dtysky@outlook.com)
+ * @Link   : dtysky.moe
+ * @Date   : 2019/10/10 0:00:00PM
+ */
+using System;
+using System.Collections.Generic;
+using GLTF.Schema;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace SeinJS
+{
+    public class Sein_processedExtensionFactory : SeinExtensionFactory
+    {
+        public override string GetExtensionName() { return "SEIN_processed"; }
+
+        public override void Serialize(ExporterEntry entry, Dictionary<string, Extension> extensions, UnityEngine.Object component = null, object options = null)
+        {
+            AddExtension(extensions, new Sein_processedExtension());
+        }
+
+        public override Extension Deserialize(GLTFRoot root, JProperty extensionToken)
+        {
+            return new Sein_processedExtension();
+        }
+    }
+}
